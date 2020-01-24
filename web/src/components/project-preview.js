5b5dfb201347c40da3f301b13dd5b795de486332
Jesper Strange Klitgaard Christiansen <jesperkc@gmail.com>
2020-01-24 19:58:24 +0100

    Vote grid

--- web/src/components/project-preview.js
+++ web/src/components/project-preview.js
@@ -1,34 +1,30 @@
-import {Link} from 'gatsby'
-import React from 'react'
-import {cn, buildImageObj} from '../lib/helpers'
-import {imageUrlFor} from '../lib/image-url'
-import BlockText from './block-text'
+import { Link } from "gatsby";
+import React from "react";
+import { cn, buildImageObj } from "../lib/helpers";
+import { imageUrlFor } from "../lib/image-url";
+import BlockText from "./block-text";
 
-import styles from './project-preview.module.css'
-import {responsiveTitle3} from './typography.module.css'
+import styles from "./project-preview.module.css";
+import { responsiveTitle3 } from "./typography.module.css";
 
-function ProjectPreview (props) {
-  return (
-    <Link className={styles.root} to={`/project/${props.slug.current}`}>
-      <div className={styles.leadMediaThumb}>
-        {props.mainImage && props.mainImage.asset && (
-          <img
-            src={imageUrlFor(buildImageObj(props.mainImage))
-              .width(600)
-              .height(Math.floor((9 / 16) * 600))
-              .url()}
-            alt={props.mainImage.alt}
-          />
-        )}
+class ProjectPreview extends React.Component {
+  vote() {
+    fetch(
+      "https://sanity-gatsby-portfolio-web-s8ps7nxi.netlify.com/.netlify/functions/hello"
+    ).then(res => res.json());
+  }
+  render() {
+    const { name, votes } = this.props;
+    return (
+      <div className={styles.root}>
+        <h3 className={cn(responsiveTitle3, styles.title)}>{name}</h3>
+        <p>Votes: {votes}</p>
+        <button className="btn" onClick={this.vote}>
+          Vote
+        </button>
       </div>
-      <h3 className={cn(responsiveTitle3, styles.title)}>{props.title}</h3>
-      {props._rawExcerpt && (
-        <div className={styles.excerpt}>
-          <BlockText blocks={props._rawExcerpt} />
-        </div>
-      )}
-    </Link>
-  )
+    );
+  }
 }
 
-export default ProjectPreview
+export default ProjectPreview;
